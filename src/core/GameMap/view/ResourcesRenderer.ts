00f7491edc1c8959269c92a74903dac2ff1727e3
Anton Rusinov <13165540+RusinovAnton@users.noreply.github.com>
2023-04-25 05:05:28 +0300

    fix resource emitter

--- src/core/GameMap/view/ResourcesRenderer.ts
+++ src/core/GameMap/view/ResourcesRenderer.ts
@@ -7,59 +7,47 @@ const EMIT_SPEED = 400;
 const CONSUME_SPEED = 500;
 
 export class ResourcesRenderer extends Renderer<SVGElement> {
-  pathResources = [];
-
-  emitResource(start: Vertice) {
-    const distance = 0;
-
-    const RESOURCE_ITEM_COLOR = '#031450';
-    const RESOURCE_ITEM_SIZE = 0.3;
-    const ball = createSVGElement('circle', {
-      fill: RESOURCE_ITEM_COLOR,
-      r: RESOURCE_ITEM_SIZE,
-      cx: start.x,
-      cy: start.y,
-    });
-
-    const resourceItem = {
-      index: 0,
-      element: ball,
-      distance,
-    };
-
-    const length = this.pathResources.push(resourceItem);
-    resourceItem.index = length - 1;
-
-    requestAnimationFrame(() => {
-      this.root.appendChild(ball);
-    });
-
-    return resourceItem;
-  }
-
-  emitIntervalId = null;
-
-  // TODO:
-  // This is a prototype func
   moveResourcesAlongPath(event) {
     const path = event.target;
-    this.pathResources = [];
+    const pathResources = [];
     const pathLength = path.getTotalLength();
-
     const start = path.getPointAtLength(0);
 
-    this.emitIntervalId = setInterval(() => {
-      if (this.pathResources.length > pathLength) {
-        clearInterval(this.emitIntervalId);
+    const emitIntervalId = setInterval(() => {
+      if (pathResources.length > pathLength) {
+        clearInterval(emitIntervalId);
         return;
       }
-      const item = this.emitResource(start);
-      requestAnimationFrame(() => animate(item));
+      const distance = 0;
+
+      const RESOURCE_ITEM_COLOR = '#031450';
+      const RESOURCE_ITEM_SIZE = 0.3;
+      const ball = createSVGElement('circle', {
+        fill: RESOURCE_ITEM_COLOR,
+        r: RESOURCE_ITEM_SIZE,
+        cx: start.x,
+        cy: start.y,
+      });
+
+      const resourceItem = {
+        index: 0,
+        element: ball,
+        distance,
+      };
+
+      const length = pathResources.push(resourceItem);
+      resourceItem.index = length - 1;
+
+      requestAnimationFrame(() => {
+        this.root.appendChild(ball);
+      });
+
+      requestAnimationFrame(() => animate(resourceItem));
     }, EMIT_SPEED);
 
     const animate = (item) => {
       const nextIndex = item.index - 1;
-      const nextItem = this.pathResources[nextIndex];
+      const nextItem = pathResources[nextIndex];
 
       if (nextItem) {
         const nextRect = nextItem.element.getBoundingClientRect();
